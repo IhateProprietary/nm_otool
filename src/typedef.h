0285565c497ef23fa773347dc6c2a48f007f4381
IhateProprietary <death2511@hotmail.com>
2019-02-03 00:41:42 +0100

    norm42,

--- src/typedef.h
+++ src/typedef.h
@@ -13,35 +13,48 @@
 #ifndef TYPEDEF_H
 # define TYPEDEF_H
 
-# define T_ASD(x) x##_t
+# define TYPEDEF(x, y) typedef struct x y##_t
 
-# define LOKEK(x) typedef struct x x##_t
+# define STAT_T TYPEDEF(stat, stat)
+# define MACH_HDR_T TYPEDEF(mach_header, mach_hdr)
+# define MACH_HDR64_T TYPEDEF(mach_header64, mach_hdr64)
+# define SEG_CMD_T TYPEDEF(segment_command, seg_cmd)
+# define SEG_CMD64_T TYPEDEF(segment_command64, seg_cmd64)
+# define SECT_T TYPEDEF(section, sect)
+# define SECT64_T TYPEDEF(section64, sect64)
+# define LD_CMD_T TYPEDEF(load_command, ld_cmd)
+# define ST_CMD_T TYPEDEF(symtab_command, st_cmd)
+# define NLST64_T TYPEDEF(nlist_64, nlst64)
+# define NLST_T TYPEDEF(nlist, nlst)
+# define FARCH_T TYPEDEF(fat_arch, farch)
+# define RANLIB_T TYPEDEF(ranlib, ranlib)
+# define AR_HDR_T TYPEDEF(ar_hdr, ar_hdr)
+# define MHFILE_T TYPEDEF(s_mach_file, mhfile)
+# define MSUB_T TYPEDEF(s_mach_sub, msub)
+# define MSYMS_T TYPEDEF(s_mach_syms, msyms)
+# define MTEXT_T TYPEDEF(s_mach_text, mtext)
 
-# define ASD LOKEK(stat)
+STAT_T;
+MACH_HDR_T;
+MACH_HDR64_T;
+SEG_CMD_T;
+SEG_CMD64_T;
+SECT_T;
+SECT64_T;
+LD_CMD_T;
+ST_CMD_T;
+NLST64_T;
+NLST_T;
+FARCH_T;
+RANLIB_T;
+AR_HDR_T;
 
-ASD;
-
-typedef struct stat					T_ASD(stat);
-typedef struct mach_header			mach_hdr_t;
-typedef struct mach_header_64		mach_hdr64_t;
-typedef struct segment_command		seg_cmd_t;
-typedef struct segment_command_64	seg_cmd64_t;
-typedef struct section				sect_t;
-typedef struct section_64			sect64_t;
-typedef struct load_command			ld_cmd_t;
-typedef struct symtab_command		st_cmd_t;
-typedef struct nlist_64				nlst64_t;
-typedef struct nlist				nlst_t;
-typedef struct fat_arch				farch_t;
-typedef struct ranlib				ranlib_t;
-typedef struct ar_hdr				ar_hdr_t;
-
-typedef struct s_mach_file			mhfile_t;
-typedef struct s_mach_sub			msub_t;
-typedef struct s_mach_syms			msyms_t;
+MHFILE_T;
+MSUB_T;
+MSYMS_T;
 # ifdef OTOOL
 
-typedef struct s_mach_text			mtext_t;
+MTEXT_T;
 
 # endif
 
